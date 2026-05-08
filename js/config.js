dab343dd9db82effcf70acb961eb82bf1b05f48a
Vinutha <vinutha230404@gmail.com>
2026-05-08 09:57:20 +0530

    Updated config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_nPi4ACr2g', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4t9vdcmhjske1hl28hflc8bjti', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_eokGWDuwf', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '47tnd2ead2c5hh8hp46pd88kb6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
